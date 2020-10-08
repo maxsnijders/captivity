c9c0dc67d4e673e758ab3a875d568ddb4c53b27d
Max Snijders <max.snijders@gmail.com>
2020-10-08 21:11:49 +0200

    Feature/initial version (#1)
    * Basic PoC
    
    * Added horizontal concat check
    
    * Added omitted f-flag
    
    * Added patch-default functionality
    
    * Cleaned up code
    
    * Added misaligned vertical concat check
    
    * Added basic README
    
    * Added docstrings to package
    
    * Explicit suffixes to merge bypass duplicate column check
    
    * Readme cleanup
    
    * Readme cleanup
    
    * Readme cleanup
    
    Co-authored-by: Max Snijders <snijders.max@bcg.com>

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="captivity",
+    version="0.1",
+    description="Cages in Pandas",
+    long_description=long_description,
+    author="Max Snijders",
+    author_email="max@msnijders.com",
+    copyright="Max Snijders",
+    packages=setuptools.find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    install_requires=["methodtools==0.1.2", "pyyaml==5.3.1"],
+)
