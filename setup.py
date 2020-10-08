b29642679289bff529287acb589db5ea49003eff
Max Snijders <max.snijders@gmail.com>
2020-10-08 21:33:28 +0200

    Configured setup.py for PyPi upload (#2)
    Co-authored-by: Max Snijders <snijders.max@bcg.com>

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="captivity",
-    version="0.1",
+    version="0.1.1",
     description="Cages in Pandas",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Max Snijders",
     author_email="max@msnijders.com",
     copyright="Max Snijders",
+    url="https://github.com/maxsnijders/captivity",
     packages=setuptools.find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
     ),
